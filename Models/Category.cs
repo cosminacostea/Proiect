03a4cfeafc6498e449f7f409f842d5781564e4b8
COSMINA-DENISA COSTEA <cosmina.costea@stud.ubbcluj.ro>
2021-01-09 13:00:00 +0200

    Edit project files.

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Proiect.Models
 {
     public class Category
     {
         public int ID { get; set; }
+        [Display(Name = "Category Name")]
         public string CategoryName { get; set; }
         public ICollection<ClothingCategory> ClothingCategories { get; set; }
     }
